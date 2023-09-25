f9c89aa8935525e2cb04242089bc5f6ffda899ea
koen <k.j.a.koenekoop@gmail.com>
2023-09-25 14:54:37 +0200

    update home-page

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			animation: {
+				'rotate': 'rotate 10s infinite linear'
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg,var(--tw-gradient-stops))'
+			},
+			colors: {
+				theme: colors.zinc,
+				brand: colors.emerald["400"],
+				accent: colors.yellow['400']
+			},
+			fontFamily: {
+				'sans': ['Ubuntu', ...defaultTheme.fontFamily.sans]
+			},
+			keyframes: {
+				rotate: {
+					'0%': {filter: 'hue-rotate(0deg)', transform: 'rotate(0deg)'},
+					'100%': {filter: 'hue-rotate(360deg)', transform: 'rotate(360deg)'}
+				}
+			}
+		},
+	},
+	plugins: [],
+}
