13f1f21d6352e8cb8c0171a59b6480943e50d70e
koen <k.j.a.koenekoop@gmail.com>
2023-09-25 15:18:12 +0200

    add Astro workflow file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
     integrations: [tailwind({
         applyBaseStyles: false
-    })]
+    })],
+    site: 'https://kjakoenekoop.github.io'
 });
